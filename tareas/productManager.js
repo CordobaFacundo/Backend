c54a8fc8ad69820f5a65a83344101689076406dc
unknown <facundo.cordoba2012@gmail.com>
2023-02-13 23:28:39 -0300

    fileSystem

--- tareas/productManager.js
+++ tareas/productManager.js
@@ -1,71 +1,119 @@
+const fs = require('fs')
+
 class ProductManager {
     static products;
-    
-    constructor() {
-        this.products = [];
-        this.id=0;
+    id = 0;
+
+    constructor(path) {
+        this.path = path;
     }
 
-    addProducts(product) {
-        if(!product.title && !product.description && !product.price && !product.stock) {
-            console.log("Informacion del producto incompleto!");
-        }else {
+    async addProducts(product) {
+        this.products = await this.getProducts();
+        this.id = this.products.length;
+
+        if (!product.title || !product.description || !product.price || !product.stock) {
+            console.log("Informacion del producto incompleto");
+        } else if (this.id === 0) {
+            // this.pushProduct(product)
+            this.id++;
+            product = { ...product, id: this.id };
+            const nuevos = [...this.products, product];
+            const nuevosStr = JSON.stringify(nuevos);
+            await fs.promises.writeFile(this.path, nuevosStr);
+            console.log(`${product.title} - agregado a la lista`);
+        } else {
             let searchCode = this.products.find((prod) => prod.code === product.code);
-            if(searchCode) {
-                console.log("Error! Codigo repetido.");
-            }else {
+            if (searchCode) {
+                console.error("Error! Codigo repetido.");
+            } else {
+                // this.pushProduct(product)
                 this.id++;
-                product = {...product, id: this.id};
-                this.products.push(product);
-                console.log(`${product.title} - agregado a la lista!`);
+                product = { ...product, id: this.id };
+                const nuevos = [...this.products, product];
+                const nuevosStr = JSON.stringify(nuevos);
+                await fs.promises.writeFile(this.path, nuevosStr);
+                console.log(`${product.title} - agregado a la lista`)
             }
         }
     }
 
-    getProducts() {
-        if(this.products.length != 0) {
-            console.log("PRODUCTOS: ", this.products);
-        }else {
-            console.log("No hay productos");
+    // async pushProduct(product) {
+    //     this.id++;
+    //     product = { ...product, id: this.id };
+    //     const nuevos = [...this.products, product];
+    //     const nuevosStr = JSON.stringify(nuevos);
+    //     await fs.promises.writeFile(this.path, nuevosStr);
+    //     console.log(`${product.title} - agregado a la lista`)
+    // }
+
+    async getProducts() {
+        try {
+            const data = await fs.promises.readFile(this.path);
+            return JSON.parse(data);
+        } catch (error) {
+            console.error(error)
+            return [];
         }
-        return this.products;
     }
 
     getProductById(id) {
         let prodForId = null;
         this.products.findIndex((prod) => {
-            if(prod.id === id) {
+            if (prod.id === id) {
                 prodForId = prod;
             }
         });
-        if(prodForId) {
+        if (prodForId) {
             console.log("El producto con id " + id + " es " + prodForId.title);
-        }else {
+        } else {
             console.log("Id not found");
         }
     }
+
+    async updateProducts(id, obj) {
+        if (!obj.title || !obj.description || !obj.price || !obj.stock) {
+            console.log("Informacion del producto a actualizar incompleta");
+        } else {
+            let searchCode = this.products.find((prod) => prod.code === obj.code);
+            if (searchCode) {
+                console.log("Error! Codigo repetido.");
+            } else {
+                const newProd = { ...obj, id: id }
+                this.products[id - 1] = newProd;
+                const prodStr = JSON.stringify(this.products);
+                await fs.promises.writeFile(this.path, prodStr);
+                console.log(`Actualizado el producto con id: ${id}`);
+            }
+        }
+    }
+
+    async deleteProduct(id) {
+        const removedProduct = { description: "Producto eliminado", id: id };
+        this.products[id - 1] = removedProduct;
+        const prodStr = JSON.stringify(this.products);
+        await fs.promises.writeFile(this.path, prodStr);
+        console.log(`Producto con id: ${id} eliminado.`)
+    }
 }
 
-const manager = new ProductManager();
 
 //Productos a agregar
 const product1 = { title: "ASUS NVIDIA GeForce RTX 3050", description: "Placa de video", price: 130000, thumbnail: "link", code: "ABC123", stock: 50 };
-manager.addProducts(product1);
-
 const product2 = { title: "Gigabyte NVIDIA GeForce RTX 3050", description: "Placa de video", price: 150000, thumbnail: "link", code: "ABC124", stock: 40 };
-manager.addProducts(product2);
-
 const product3 = { title: "Intel Core I3 10100F", description: "Procesador", price: 30000, thumbnail: "link", code: "ABC125", stock: 65 };
-manager.addProducts(product3);
+const product4 = { thumbnail: "link", code: "ABC" };
 
-// //Producto con codigo repetido
-const product4 = { title: "Intel Core I3 10100F", description: "Procesador", price: 30000, thumbnail: "link", code: "ABC125", stock: 65 };
-manager.addProducts(product4);
-
-// //Producto incompleto
-const product5 = { thumbnail: "link", code: "ABC" };
-manager.addProducts(product5);
-
-//Muestra todos los productos añadidos
-manager.getProducts();
-manager.getProductById(2);
+async function main() {
+    const manager = new ProductManager("./products.json");
+    await manager.addProducts(product1);
+    await manager.addProducts(product2);
+    await manager.addProducts(product3);
+    await manager.addProducts(product4);
+    await manager.deleteProduct(1);
+    await manager.updateProducts(1, { title: "Redragon Kumara K552", description: "Teclado", price: 15000, thumnail: "link", code: "123", stock: 70 });
+    manager.getProductById(2);
+    const dataOnFile = await manager.getProducts();
+    console.log(dataOnFile);
+}
+main();
